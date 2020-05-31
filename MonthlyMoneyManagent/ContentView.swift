54852a29b1152ba748243911e76d572b1b081d58
CountDer3k <der3kburrola@gmail.com>
2020-05-31 15:42:00 -0600

    Initial Commit

---
+++ MonthlyMoneyManagent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MonthlyMoneyManagent
+//
+//  Created by Der3k Burrola on 5/31/20.
+//  Copyright © 2020 D3Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
